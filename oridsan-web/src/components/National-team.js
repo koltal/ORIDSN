cc3f0b4e6e0ebb1f26e1c043b98ebfc27a181438
koltal <nibrasabubakar@gmail.com>
2023-03-25 23:09:59 +0100

    past-leaders-card added

--- oridsan-web/src/components/National-team.js
+++ oridsan-web/src/components/National-team.js
@@ -54,11 +54,41 @@ const NationalTeam = () => {
                 </div>
                 <div className="National-Executive-Card">
                   <div>
-                  <img src="https://nacos.org.ng/img/president.jpg" alt=""/>
+                  <img  src="https://nacos.org.ng/img/president.jpg" alt=""/>
                   </div>
                     <div className="Name-position-section">
                     <center><h3>Dr Umar Muhammad Modibbo phD</h3>
                     <p>National President</p></center>
+                    </div>
+                </div>
+            </div>
+            <div><center><h2>Past National President</h2></center></div>
+            <div className="National-Executive-Team-Container">
+                <div className="National-Executive-Card">
+                  <div>
+                  <img src="https://nacos.org.ng/img/jerome_SEC.jpg" alt=""/>
+                  </div>
+                    <div className="Name-position-section">
+                    <center><h3>Mr Muhammad Modibbo phD</h3>
+                    <p>North East (2007-2011)</p></center>
+                    </div>
+                </div>
+                <div className="National-Executive-Card">
+                  <div>
+                  <img src="https://nacos.org.ng/img/finsec.jpg" alt=""/>
+                  </div>
+                    <div className="Name-position-section">
+                    <center><h3>Mr Hilary Welberforce</h3>
+                    <p>North East (2011-2015)</p></center>
+                    </div>
+                </div>
+                <div className="National-Executive-Card">
+                  <div>
+                  <img src="https://nacos.org.ng/img/hadiza.jpg" alt=""/>
+                  </div>
+                    <div className="Name-position-section">
+                    <center><h3>Mrs Ketherine Josephine</h3>
+                    <p>North East (2015-Date)</p></center>
                     </div>
                 </div>
             </div>
