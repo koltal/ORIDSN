7eeee125852447511da0e8f646821e2af8f5fdff
koltal <nibrasabubakar@gmail.com>
2023-03-25 15:52:01 +0100

    about-fixed

--- oridsan-web/src/pages/about.js
+++ oridsan-web/src/pages/about.js
@@ -4,24 +4,25 @@ const About = () => {
     return ( 
         <div>
            <div className='About-Section-Container'>
-            <div><h2>---About ORIDSAN---</h2></div>
+            <div><h2>--ABOUT ORIDSAN--</h2></div>
 
-            <div>
+            
+           </div>
+           <div className='History-Container'>
                 <div>
                     <h3>History of ORIDSAN</h3>
                     <div>
                         <p><ReadMore>Despite the diversity of perspectives of OR, its unique activity, which informs the kind of basic training, is fundamentally the attack of science on operational problems existing in any endeavour, civilian or military, Business, politics, industry, general administration, or even in self-management. This attack on science originates in the activities of the pioneers of OR.
-	Indeed, literature is replete with tracing the origin of formal OR to the World War II exigencies that compelled enlisting the contribution of active scientists in fighting the war. Science’s gift to humanity is research, which is the bedrock of developments over the ages, especially in modern times. It was this research that scientists during the World War faithful brought to bear on military operations of the time, which yielded sounding results that recommended a formalization of such activities and their spread to other endeavours, even after the war. Thus, two values are fundamental to OR: Its science bases, pervasive implication, and applicability.
-	Science is fundamentally about systematically searching for facts (information gathering and data recording), and organizing such facts (data) to deduce further attributes that are factually verified and valid. This is what Research is all about. Our field, OR, seeks faithfully apply this approach and methodology to operations in any concept. The use of scientific research in tackling formalized operational problems in diverse situations since World War II has led to the formalization of the experiences into what we now know as OR problem models and solution techniques, most of which are mathematical. Thus, OR today comprises:
-1.Its philosophy of factually and systematically analyzing operations to tackle them.
-2.Its methodology of formally representing the such problem as the basis for reasoning out the solutions.
-3.Its techniques are special procedures, usually derived from appropriate mathematical theories, as the tools for deducing solutions to the problems using the models.
-4.Application, which pervasively is an operation in any situation.
-It is clear from the above that OR has something for everybody. You may be a sophisticated mathematician and yet be a significant Operational Researcher.  </ReadMore></p>
+                        Indeed, literature is replete with tracing the origin of formal OR to the World War II exigencies that compelled enlisting the contribution of active scientists in fighting the war. Science’s gift to humanity is research, which is the bedrock of developments over the ages, especially in modern times. It was this research that scientists during the World War faithful brought to bear on military operations of the time, which yielded sounding results that recommended a formalization of such activities and their spread to other endeavours, even after the war. Thus, two values are fundamental to OR: Its science bases, pervasive implication, and applicability.
+                        Science is fundamentally about systematically searching for facts (information gathering and data recording), and organizing such facts (data) to deduce further attributes that are factually verified and valid. This is what Research is all about. Our field, OR, seeks faithfully apply this approach and methodology to operations in any concept. The use of scientific research in tackling formalized operational problems in diverse situations since World War II has led to the formalization of the experiences into what we now know as OR problem models and solution techniques, most of which are mathematical. Thus, OR today comprises:
+                    1.Its philosophy of factually and systematically analyzing operations to tackle them.
+                    2.Its methodology of formally representing the such problem as the basis for reasoning out the solutions.
+                    3.Its techniques are special procedures, usually derived from appropriate mathematical theories, as the tools for deducing solutions to the problems using the models.
+                    4.Application, which pervasively is an operation in any situation.
+                    It is clear from the above that OR has something for everybody. You may be a sophisticated mathematician and yet be a significant Operational Researcher.  </ReadMore></p>
                     </div>
                 </div>
             </div>
-           </div>
         </div>
      );
 }
