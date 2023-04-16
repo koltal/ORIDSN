c6705c5c63e8501d225aceb09ff6cacf08990045
koltal <nibrasabubakar@gmail.com>
2023-04-16 15:40:39 +0100

    pub

--- oridsan-web/src/pages/publication.jsx
+++ oridsan-web/src/pages/publication.jsx
@@ -12,9 +12,16 @@ const Publication = () => {
         <div className="Publication-Container">
             <div>
                <div> <h1>Nigerian Journal of Operations Research (NIJOR)</h1></div>
-               <div><p>The Operational Research Society of India publishes a quarterly journal OPSEARCH. The first issue of the journal was published in January 1964. The journal publishes research and application papers and short communications. The papers published in OPSEARCH journal are abstracted in International Abstracts in Operational Research (IAOR), OR/MS and other abstracting journals.
-
-                Besides the publication of OPSEARCH journal, ORSI publishes proceedings of the international/national seminars and special issues from time to time.</p></div>
+               <div>
+                <h2>Overview</h2>
+                <p>Nigerian Journal of Operations Research (NIJOR)  is the official journal of the ORIDSAN. Therefore, it is committed to advancing and promoting the understanding, theories, methodologies and practices of Operations Research in Nigeria and worldwide.</p>
+                </div>
+                <div>
+                <h2>Aim and Scope</h2>
+                <p>The Nigerian Journal of Operations Research (NIJOR) publishes high-quality, peer-reviewed original papers contributing to the field and practice of Operations Research, Management Sciences, Industrial Engineering and Decision Analytics. 
+                The NIJOR publishes articles type related to the theories, applications, and reviews in the following areas:
+                Applied Stochastic optimization, Big Data Optimization, Computational Intelligence, Decision Support System, Discrete and Continuous Optimization, Mathematical Programming, Modelling and Simulation, Multicriteria Decision Analysis, Reliability Optimization, Supply Chain and Inventory Management, Portfolio and Asset Optimization, and Sustainability Optimization</p>
+                </div>
             </div>
             <div>
                 <img src={opsearch} alt="publication" width='80%' height='500px' />
