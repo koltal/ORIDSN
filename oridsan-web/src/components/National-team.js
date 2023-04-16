aff85eedafc90e3f7446b7a5c99d33132b5a05ff
koltal <nibrasabubakar@gmail.com>
2023-04-16 15:35:15 +0100

    fixed executive cart container

--- oridsan-web/src/components/National-team.js
+++ oridsan-web/src/components/National-team.js
@@ -33,34 +33,10 @@ const NationalTeam = () => {
                     </div>
                 </div>
 
-                <div className="National-Executive-Card">
-                  <div>
-                  <img src="https://nacos.org.ng/img/president.jpg" alt=""/>
-                  </div>
-                    <div className="Name-position-section">
-                    <center><h3>Dr Umar Muhammad Modibbo phD</h3>
-                    <p>National President</p></center>
-                    </div>
-                </div>
+                
 
-                <div className="National-Executive-Card">
-                  <div>
-                  <img src="https://nacos.org.ng/img/president.jpg" alt=""/>
-                  </div>
-                    <div className="Name-position-section">
-                    <center><h3>Dr Umar Muhammad Modibbo phD</h3>
-                    <p>National President</p></center>
-                    </div>
-                </div>
-                <div className="National-Executive-Card">
-                  <div>
-                  <img  src="https://nacos.org.ng/img/president.jpg" alt=""/>
-                  </div>
-                    <div className="Name-position-section">
-                    <center><h3>Dr Umar Muhammad Modibbo phD</h3>
-                    <p>National President</p></center>
-                    </div>
-                </div>
+               
+                
             </div>
             <div><center><h2>Past National President</h2></center></div>
             <div className="National-Executive-Team-Container">
